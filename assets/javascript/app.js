405dba6267eb824ab916b5b621e2bf2a61a6f6f7
rtgiles <31195013+rtgiles@users.noreply.github.com>
2017-10-12 13:03:07 -0500

    latest update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,75 +1,162 @@
 $(document).ready(function() {
 
-//Creates start button on start-up screen
+	//Creates start button on start-up screen
 	function initScr() {
-		strtUpScr = '<button type="button" class="btn btn-info btn-lg btn-block">Start The Trivia Challenge</button>';
+		strtUpScr = '<button type="button" class="btn btn-info btn-lg btn-block start-up">Start The Trivia Challenge</button>';
 		$("#mainBlock").html(strtUpScr);
 	}
 
 	initScr();
 
+	var triviaQuestions = [{
+				question: "The movie \"The Last King of Scotland\" was about which ruler?",
+				correctOption: 1, 
+				triviaOptions: [
+					 "William of Orange",
+					 "Idi Amin Dada",
+					 "King Henry IV",
+					 "King Richard the Lionheart",
+				]},
+				{
+				question: "Which movie did Sydnie Poiter win an Academy Award for?",
+				correctOption: 2,
+				triviaOptions: [
+					 "To Sir with Love",
+					 "In the Heat of the Night",
+					 "Lilies of the Field",
+					 "The Defiant Ones",
+				]},
+				{
+				question: "Which movie won the Academy Award for best picture?",
+				correctOption: 3, 
+				triviaOptions: [
+					"The Color Purple",
+					"Cotton Comes to Harlem",
+					"Guess Who's Coming To Dinner",
+					"12 Years A Slave",
+				]},
+				{
+				question: "The movie \"The Great White Hope\" was based on the life of which professional boxer?",
+				correctOption: 0,
+				triviaOptions: [
+					"Jake Johnson",
+					"Max Snell",
+					"James Jeffries",
+					"Max Schmeling",
+				]},
+	];
+	var timer = 30;
+	var questCntr = 0;
+	var theTimer;
+	var gameTimer;
+	var correct = 0;
+	var wrong = 0;
+	var unanswered = 0;
+	var pickedAnswr;
 
+	//Timer function
+	function timerShell(){
+		theTimer = setInterval(thirtySec, 1000);
+		var gameTimer;
+		function thirtySec() {
+			if (timer === 0) {
+				clearInterval(theTimer);
+				//Generate loss do to timeRundown
+				unanswered++;
+				alert("THINK FASTER!!!");
+				nextQuestion();
+			}
+			if (timer > 0) {
+				timer--;
+				//gameTimer = $("#gameClock").append('<h2>'"Time Remaining: "'<span id="clock">' + timer + '</span>''</h2>');
+				$("#gameClock").html(timer);
+			}
+		}
+	}
+	//Function to display next set of questions or end game
+	function nextQuestion() {
+		if (questCntr < 4){
+			questCntr++;
+			timer = 30;
+			timerShell();
+			triviaInsert();
+		}
+		else{
+			endGame();
+		}
+	}
 
-var triviaQuestions = {
-	Q1: {
-		a0: "The movie \"The Last King of Scotland\" was about which ruler?",
-		a1: "",
-		a2: "",
-		a3: "",
-		a4: "",
-	},
-	Q2: {
-		a0: "Which movie did Sydnie Poiter win an Academy Award for?",
-		a1: "",
-		a2: "",
-		a3: "",
-		a4: "",
-	},
-	Q3: {
-		a0: "Which movie won the Academy Award for best picture?",
-		a1: "",
-		a2: "",
-		a3: "",
-		a4: "",
-	},
-	Q4: {
-		a0: "The movie \"The Great White Hope\" was based on the life of which professional boxer?",
-		a1: "",
-		a2: "",
-		a3: "",
-		a4: "",
-	},
-};
-var timer = 30;
-var questCntr = 0;
-var Clock;
-var correct = 0;
-var wrong = 0;
-var unanswered = 0;
+	function endGame() {
+		timer = 30;
+		questCntr = 0;
+		theTimer;
+		gameTimer;
+		correct = 0;
+		wrong = 0;
+		unanswered = 0;
+		pickedAnswr;
+	}
 
-/*function triviaInsert() {
-	var triviaBlock = {
-		<ol>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-		<ol>*/
-//or
+	// Timer run out function
+	function timeRunDown() {
+		unanswered++;
+	}
 
-function triviaInsert()	{
-	var triviaQues = '<button type="button" class="btn btn-info btn-lg btn-block"> console.log(triviaQuestions[0].a0) </button>';
-	var triviaAns1 = '<button type="button" class="btn btn-info btn-md btn-block"> .triviaQuestions[0].a1 </button>';
-	var triviaAns2 = '<button type="button" class="btn btn-info btn-md btn-block"> .triviaQuestions[0].a2 </button>';
-	var triviaAns3 = '<button type="button" class="btn btn-info btn-md btn-block"> .triviaQuestions[0].a2 </button>';
-	var triviaAns4 = '<button type="button" class="btn btn-info btn-md btn-block"> .triviaQuestions[0].a2 </button>';
-	$("#mainBlock").html(triviaQues + triviaAns1 + triviaAns2 + triviaAns3 + triviaAns4);	
-}
+	// Button Generator
+	function triviaInsert()	{
+		$("#mainBlock").html("");
+		$("#mainBlock").append();
+		for (var key in triviaQuestions[questCntr])	{
+			//console.log(triviaQuestions[0])
+			//var triviaBtn = '<button type="button" class="btn btn-info btn-lg btn-block">' + triviaQuestions[0][key] +'</button>';
+			var triviaBtn;
+			if (Array.isArray(triviaQuestions[questCntr][key])) {
+				for(var i = 0; i < triviaQuestions[questCntr][key].length; i++){
+					triviaBtn = $("#mainBlock").append('<button type="button" class="btn btn-info btn-md btn-block answer">' + triviaQuestions[questCntr][key][i] +'</button>')
+					triviaBtn.attr("data-answer-index","i");
+					$("#mainBlock").append(triviaBtn);
+				}
+			}
+			else {
+				triviaBtn = '<button type="button" class="btn btn-info btn-lg btn-block">' + triviaQuestions[questCntr][key] +'</button>'
+				$("#mainBlock").append(triviaBtn);
+			}
+		}	
+	}
+	$("body").on("click", ".start-up", function(event){	
+		event.preventDefault();
+		triviaInsert();
+		timerShell();
+	});
 
-$("body").on('click', function(){	
-triviaInsert();
-})
+	//answer function
+	$("body").on("click", ".answer", function(event){
+		pickedAnswr = $(this).text();
+		pickedAnswrIndex = $(this).attr("data-answer-index");
+		console.log("pickedAnswr " + pickedAnswr);
+		console.log("pickedAnswrIndex " + pickedAnswrIndex);
+		console.log("triviaOptions " +triviaQuestions[questCntr].triviaOptions );
+		console.log("correctOption "+triviaQuestions[questCntr].correctOption);
 		
+		if (pickedAnswr == triviaQuestions[questCntr].correctOption){
+			alert("Way To Go Braniac");
+			nextQuestion();
+			}
+			// Reset Timer
+			// Increment Win Counter
+			else {
+				alert("Back to the movies for You");
+				nextQuestion();
+			//Reset Timer
+			//Increment Win Counter
+			}
+		})
+	});//end answer function		
 	
 
-})
+
+
+	
+
+
+
